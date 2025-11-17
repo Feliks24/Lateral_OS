b392df12eb37d0d0523be1ae1346f2e41ac040fd
Bennet Worrmann <worrmab99@andorra.imp.fu-berlin.de>
2025-11-17 16:50:24 +0100

    checkpoint remap

--- lateral_OS/start.c
+++ lateral_OS/start.c
@@ -1,11 +1,11 @@
 #include "debug_utils.h"
+#include "remap_utils.h"
 
-
-
+// TASK 01
+/*
 __attribute__((naked, section(".init"))) void _start(void)
 {
 	// start point of the system
-
 
 	unsigned int char1 = 97; //352 mod 255 = 97
 	unsigned int char2 = 353; //352 mod 255 = 97
@@ -22,5 +22,22 @@ __attribute__((naked, section(".init"))) void _start(void)
 		pprintf("Read input: %c\n", c);
 	}
 }
+*/
 
+// TASK 02
+__attribute__((naked, section(".init"))) void _start(void)
+{
+	// step 1: remap memory
+	memory_remap();
 
+	// step 2: IVT
+
+	// step 3: stack
+
+	// step 4: test handler
+
+	for (;;)
+	{
+		;
+	}
+}
