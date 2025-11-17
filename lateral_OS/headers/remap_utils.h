5d30b6bfc18eb72245d8fb9fc08267a000447b71
polluxien <bennet@worrmann.de>
2025-11-17 16:57:16 +0100

    added c files

---
+++ lateral_OS/headers/remap_utils.h
@@ -0,0 +1,6 @@
+#ifndef _REMAP_UTILS_H_
+#define _REMAP_UTILS_H_
+
+void memory_remap(void);
+
+#endif
---
+++ lateral_OS/utils/thread.c
@@ -0,0 +1,9 @@
+// ? Versuch Exception Handler zu initalisieren
+
+// ? Prozesseor Modus - Kernel (priviligiert)
+#define EXCEPTION_MODUS_FIQ 
+#define EXCEPTION_MODUS_IRQ
+#define EXCEPTION_MODUS_SUPERVISOR
+#define EXCEPTION_MODUS_ABORT
+#define EXCEPTION_MODUS_UNDEFINED
+#define EXCEPTION_MODUS_SYSTEM
