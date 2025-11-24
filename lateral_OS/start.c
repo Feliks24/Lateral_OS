c2af3cf8fb07c8818c36338fd2ffe9b6d88100c9
polluxien <bennet@worrmann.de>
2025-11-24 19:07:43 +0100

    also user funktioniert nicht mmmmmmh

--- lateral_OS/start.c
+++ lateral_OS/start.c
@@ -2,8 +2,7 @@
 #include "system.h"
 #include "cosmetic.h"
 
-__attribute__((naked, section(".init")))
-void _start(void)
+__attribute__((naked, section(".init"))) void _start(void)
 {
 	_print_titel();
 
@@ -17,15 +16,44 @@ void _start(void)
 	init_stack();
 
 	// step 4: test handler
-	//*(int *) 0x00400029 = 0;
-	lprintf("now testing software interrupt");
-	asm("swi #0");
+	lprintf("Choose a exception to test the functionality \n");
+	lprintf("1.........................Software Interrupt \n");
+	lprintf("2.................................Data Abort \n");
+	lprintf("3......................Undefined Instruction \n");
 
-	const char *our_string = "testing";
-	lprintf("this string: %s\n", our_string);
-
-	for (;;)
+	while (1)
 	{
-		;
+		char c = char_get();
+		lprintf("Read input: %c\n", c);
+
+		switch (c)
+		{
+		case '1':
+			/*
+			 * Software Interrupt (siehe Kap. A4.1.107 im ARM
+			 * Architecture Reference Manual)
+			 */
+			asm("swi #0");
+			break;
+		case '2':
+			/* Data Abort (siehe Kap. 8 im AT91RM9200 Handbuch) */
+			*(int *)0xa0000000 = 0;
+			break;
+		case '3':
+			/*
+			 * Undefined Instruction (aus dem "Architecturally
+			 * Undefined Instruction space", siehe Kap. A3.16.5 im
+			 * ARM Architecture Reference Manual)
+			 */
+			asm(".word 0x07F000F0");
+			break;
+		default:
+			continue;
+		}
+
+		lprintf("\n"
+			   "Unerwartete Rückkehr?! (Data Abort geht unter QEMU nicht.)\n"
+			   "Was darf es jetzt sein?\n"
+			   "> ");
 	}
 }
--- lateral_OS/system/init_stack.c
+++ lateral_OS/system/init_stack.c
@@ -1,7 +1,7 @@
 #include "utils.h"
 
-#define STACK_SIZE (8192*4)
-#define STACK_START 0x00300000-(STACK_SIZE*8)
+#define STACK_SIZE (8192 * 4)
+#define STACK_START 0x00300000 - (STACK_SIZE * 8)
 
 #define USR_BIT 0b10000
 #define FIQ_BIT 0b10001
@@ -11,41 +11,37 @@
 #define UND_BIT 0b11011
 #define SYS_BIT 0b11111
 
+void _init_stack(unsigned int mode, unsigned int address);
 
-void _init_stack(unsigned int mode, unsigned int address); 
-
-
-
-void singular_stack_set(unsigned int mode,void * address)
+void singular_stack_set(unsigned int mode, void *address)
 {
-	//r0 -> mode
-	//r1 -> address
-
+	// r0 -> mode
+	// r1 -> address
 
 	asm volatile(
 		"mrs r2, cpsr\n"
 		"bic r3,r2, #0x1f\n"
-		"orr r3,r3,%0\n" 
+		"orr r3,r3,%0\n"
 		"msr cpsr_c, r3\n"
-		"mov sp, %1\n" 
+		"mov sp, %1\n"
 		"msr cpsr_c, r2\n"
-		"mov pc, lr\n"
-		:: "r" (mode), "r" (address)
-	);
-
+		"mov pc, lr\n" ::"r"(mode),
+		"r"(address));
 }
 
 void init_stack(void)
 {
-	
-	int modes[] ={FIQ_BIT, IRQ_BIT, SVC_BIT, ABT_BIT,UND_BIT,SYS_BIT, USR_BIT};
+	// ! USR funktioniert nicht
+	int modes[] = {FIQ_BIT, IRQ_BIT, SVC_BIT, ABT_BIT, UND_BIT, SYS_BIT, USR_BIT};
+	char *mode_names[] = {"FIQ", "IRQ", "SVC", "ABT", "UND", "USR"};
+
 	unsigned int i;
-	for(i=0; i<7; i++){
-		_init_stack(modes[i],(unsigned int) STACK_START +i*STACK_SIZE);
-		lprintf("stack set for mode %p\n", modes[i]);
+	for (i = 0; i < 6; i++)
+	{
+		unsigned int addr = STACK_START - i * STACK_SIZE;
+		_init_stack(modes[i], addr);
+		lprintf("stack set for mode %s : %x : %x \n", mode_names[i], modes[i], addr);
 	}
 	lprintf("end of stack init");
 	return;
 }
-
-
